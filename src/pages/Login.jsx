d6e29872342729e8b762e762aba519c2961d1da6
Paul <paulventer87@gmail.com>
2022-07-05 18:57:27 +0200

    Added styling to login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,17 +3,19 @@ import { useState } from "react";
 import Spinner from "../components/Spinner";
 import { useNavigate, } from 'react-router-dom';
 import { server } from '../config/production';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");  
   const navigate = useNavigate();
+ 
 
 
-  async function register(e) {
-    e.preventDefault();
-
+  async function login() {   
     try {
       const loginData = {
         email,
@@ -21,40 +23,57 @@ function Login() {
       };
 
       const res = await axios.post(`${server}/auth/login`, loginData, {withCredentials: "True", Credentials: 'include'});
-
-      console.log(res);
-      
+     
       setEmail("")
       setPassword("")      
-      console.log("Logged in succesfully")
+      
       navigate("/")
     } catch (err) {
-      console.error(err);
-      console.log("Logged in failed")
+      console.error(err);      
     }
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={register}>
-        <input
-          type="email"
-          placeholder="Email"
+    <div className="login-container">
+      <div className="form-container">
+        <h1>Login</h1>        
+        <Box
+          component="form"
+          className="input-container"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
           autoComplete="off"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          autoComplete="off"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />        
+        >
+          <TextField 
+            className="input-field"
+            id="outlined-basic" 
+            label="Email" 
+            variant="outlined" 
+            type="email"
+            placeholder="Email"        
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <TextField 
+            className="input-field"
+            id="outlined-basic" 
+            label="Password" 
+            variant="outlined" 
+            type="password"
+            placeholder="Password"        
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <Button variant="contained" size="medium" onClick={login}>
+           Login
+          </Button>          
+        </Box>
+        <span>Don't have an account?</span>
+        <a href="/register">Register</a>
         
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
 import { useState } from "react";
 import { server } from '../config/production';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useNavigate, } from 'react-router-dom';
 
 
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVerify, setPasswordVerify] = useState("");  
+  const navigate = useNavigate();
 
-  async function register(e) {
-    e.preventDefault();
+  async function register() {    
 
     try {
       const registerData = {
@@ -24,38 +28,62 @@ function Register() {
       setPassword("")
       setPasswordVerify("")
       
+      navigate("/")
     } catch (err) {
       console.error(err);
     }
   }
 
   return (
-    <div>
-      <h1>Register a new account</h1>
-      <form onSubmit={register}>
-        <input
-          type="email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
+    <div className="login-container">
+      <div className="form-container">
+        <h1>Register an account</h1>
+        <Box
+          component="form"
+          className="input-container"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
           autoComplete="off"
-          value={email}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          autoComplete="off"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <input
-          type="password"
-          placeholder="Verify your password"
-          autoComplete="off"
-          onChange={(e) => setPasswordVerify(e.target.value)}
-          value={passwordVerify}
-        />
-        <button type="submit">Register</button>
-      </form>
+        >
+          <TextField 
+            className="input-field"
+            id="outlined-basic" 
+            label="Email" 
+            variant="outlined" 
+            type="email"
+            placeholder="Email"        
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <TextField 
+            className="input-field"
+            id="outlined-basic" 
+            label="Password" 
+            variant="outlined" 
+            type="password"
+            placeholder="Password"        
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <TextField 
+            className="input-field"
+            id="outlined-basic" 
+            label="Verify Password" 
+            variant="outlined" 
+            type="password"
+            placeholder="Verify Password"        
+            onChange={(e) => setPasswordVerify(e.target.value)}
+            value={passwordVerify}
+          />
+          <Button variant="contained" size="medium" onClick={register}>
+          Login
+          </Button>          
+        </Box>
+        <span>Already have an account?</span>
+        <a href="/login">Login</a>
+      </div>
     </div>
   );
 }
